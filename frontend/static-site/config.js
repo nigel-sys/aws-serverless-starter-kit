a78a4880dec810877e57dd5a9502589d2da76885
Nigel <pratyushnbaxla79@gmail.com>
2025-07-17 22:45:14 +0100

    Add placeholders in config.js instead of real values

--- frontend/static-site/config.js
+++ frontend/static-site/config.js
@@ -1,8 +1,9 @@
 window.APP_CONFIG = {
-  apiUrl: 'https://khz5e0eygg.execute-api.us-east-1.amazonaws.com/prod/hello',
-  clientId: '55403cj4qov65aesp1ri848jeq',
+  apiUrl:
+    'https://your-api-id.execute-api.your-region.amazonaws.com/prod/hello',
+  clientId: 'your-cognito-app-client-id',
   cognitoDomain:
-    'https://serverless-starter-auth-domain-577d5819.auth.us-east-1.amazoncognito.com',
-  redirectUri: 'https://d2h6dh0ek3z5t8.cloudfront.net/callback.html',
-  logoutRedirectUri: 'http://d2h6dh0ek3z5t8.cloudfront.net/index.html',
+    'https://your-cognito-domain.auth.your-region.amazoncognito.com',
+  redirectUri: 'https://your-cloudfront-url/callback.html',
+  logoutRedirectUri: 'https://your-cloudfront-url/index.html',
 };
