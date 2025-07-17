b5dfb8cfacbfbbb468709df52b26e0b775cbe9c9
Nigel <pratyushnbaxla79@gmail.com>
2025-07-17 22:23:51 +0100

    Fix  config.js

--- frontend/static-site/config.js
+++ frontend/static-site/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   apiUrl: 'https://khz5e0eygg.execute-api.us-east-1.amazonaws.com/prod/hello',
-  cognitoClientId: '55403cj4qov65aesp1ri848jeq',
+  clientId: '55403cj4qov65aesp1ri848jeq',
   cognitoDomain:
     'https://serverless-starter-auth-domain-577d5819.auth.us-east-1.amazoncognito.com',
   redirectUri: 'https://d2h6dh0ek3z5t8.cloudfront.net/callback.html',
